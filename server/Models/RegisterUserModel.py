51cec707afc6d6758b5dd48472df9269c2d14f43
david8219501 <8487353@gmail.com>
2024-07-24 12:25:06 +0300

    Creating the server

---
+++ server/Models/RegisterUserModel.py
@@ -0,0 +1,9 @@
+# namespace UserApi.Models
+# {
+#     public class RegisterUserModel
+#     {
+#         public string Name { get; set; }
+#         public string Email { get; set; }
+#         public string Password { get; set; }
+#     }
+# }
